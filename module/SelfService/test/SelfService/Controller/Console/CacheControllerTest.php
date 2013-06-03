73144adedd2478b92ec6f83380662ad87d97c042
Ryan J. Geyer <me@ryangeyer.com>
2013-06-03 11:24:07 -0700

    Removed unused code from cache controller test

--- module/SelfService/test/SelfService/Controller/Console/CacheControllerTest.php
+++ module/SelfService/test/SelfService/Controller/Console/CacheControllerTest.php
@@ -12,19 +12,6 @@ class CacheControllerTest extends AbstractConsoleControllerTestCase {
     parent::setUp();
 
     $serviceManager = $this->getApplicationServiceLocator();
-
-    // Initialize the schema.. Maybe I should register a module for clearing the schema/data
-    // and/or loading mock test data
-    $em = $serviceManager->get('doctrine.entitymanager.orm_default');
-    $cli = new \Symfony\Component\Console\Application("PHPUnit Bootstrap", 1);
-    $cli->setAutoExit(false);
-    $helperSet = $cli->getHelperSet();
-    $helperSet->set(new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em), 'em');
-    $cli->addCommands(array(new \Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand()));
-    $cli->run(
-      new \Symfony\Component\Console\Input\ArrayInput(array('orm:schema-tool:create')),
-      new \Symfony\Component\Console\Output\NullOutput()
-    );
   }
 
   public function testUpdateRightScaleActionCanBeAccessed() {
